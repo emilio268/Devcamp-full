c54bd916321492a26b3b62a8d2228e73e7058bb8
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-11-15 17:13:12 -1200

    actualizacion

--- routes/bootcampRoutes.js
+++ routes/bootcampRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bootcampModel = require('../models/bootcampModel')
+const { default: mongoose } = require('mongoose')
 const router = express.Router()
 
 //definir rutas de bootcamps
@@ -10,24 +11,72 @@ router.get('/', async (req, res) => {
     //Seleccionar todos los bootcamps
     //en la colección
 
-    const bootcamps = await bootcampModel.find()
+    try{
+        const bootcamps = 
+            await bootcampModel.find()
+        if(bootcamps.lenght === 0){
+            res.
+            status(400). 
+            json({
+                sucess: false,
+                msg: "no hay bootcamps en la collection"
+            })
+        }else{
+            res.
+            status(200). 
+            json({
+                sucess: true,
+                data: bootcamps
+            })
+        }
+    }catch(error){
+        res.
+            status(error.status). 
+            json({
+                sucess: false,
+                msg: error.message
+            })
+    }
 
-    res.json({
-        sucess: true,
-        result: bootcamps
-    })
 })
 
 //Seleccionar bootcamp por id
 router.get('/:id', async (req, res) => {
-    bootcampid = req.params.id
 
-    selected_Bootcamp = await bootcampModel.findById(bootcampid)
+    try{
+        bootcampid = req.params.id
 
-    res.json({
-        sucess: true,
-        results: selected_Bootcamp
-    })
+        if(!mongoose.Types.ObjectId.isValid(bootcampid)){
+            res.status(400).json({
+                sucess: false,
+                msg: "el id no es valido"
+            })
+        }else{
+            selected_Bootcamp = await bootcampModel.
+                            findById(bootcampid)
+
+        if(selected_Bootcamp){
+            //se encontro el bootcamp
+            res.status(200).json({
+                sucess: true,
+                results: selected_Bootcamp
+            })
+        }else{
+            //No se haya encontrado
+            res.status(400).json({
+                sucess: false,
+                msg: `No se encontro el bootcamp ${bootcampid}`
+            })
+        }
+        }
+        
+
+    }catch(error){
+        res.status(500).json({
+            sucess:false,
+            msg: error.message
+        })
+    }
 })
 
 //Crear bootcamp
